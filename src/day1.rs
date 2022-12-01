3b212b35403d128d2d25614e7cdf74f591ec2b52
Idan Arye <IdanArye@gmail.com>
2022-12-02 01:00:39 +0200

    Update `aoc-main`

--- src/day1.rs
+++ src/day1.rs
@@ -3,28 +3,31 @@ use itertools::Itertools;
 type RowData = Vec<usize>;
 
 pub fn generator(input: &str) -> Vec<RowData> {
-    input.lines().batching(|it| {
-        let elf_items = it.map_while(|line| {
-            if line.is_empty() {
-                None
-            } else {
-                line.parse().ok()
-            }
-        }).collect::<Vec<usize>>();
-        if elf_items.is_empty() {
-            None
-        } else {
-            Some(elf_items)
-        }
-    }).collect::<Vec<_>>()
+    input
+        .lines()
+        .batching(|it| {
+            Some(
+                it.map_while(|line| line.parse().ok())
+                    .collect::<Vec<usize>>(),
+            )
+            .filter(|list| !list.is_empty())
+        })
+        .collect::<Vec<Vec<usize>>>()
 }
 
 pub fn part_1(input: &[RowData]) -> usize {
-    input.iter().map(|elf_items| elf_items.iter().sum()).max().unwrap()
+    input
+        .iter()
+        .map(|elf_items| elf_items.iter().sum())
+        .max()
+        .unwrap()
 }
 
 pub fn part_2(input: &[RowData]) -> usize {
-    let mut elves_totals = input.iter().map(|elf_items| elf_items.iter().sum()).collect::<Vec<usize>>();
+    let mut elves_totals = input
+        .iter()
+        .map(|elf_items| elf_items.iter().sum())
+        .collect::<Vec<usize>>();
     elves_totals.sort_by_key(|&num| std::cmp::Reverse(num));
     elves_totals.iter().take(3).sum()
 }
